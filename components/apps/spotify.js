0bf23bc55a1d2de9befb5434c01747c89dafea4b
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-01-21 19:04:06 +0530

    UI changes

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZEVXbLZ52XmnySJg" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <>
+        <iframe src="https://open.spotify.com/embed/playlist/0WRYlRS9wHe2SuRELgJkPZ" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1EVKuMoAJjoTIw" className="h-full w-full"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX76Wlfdnj7AP" className="h-full w-full"></iframe>
+        </>
     )
 }
 
