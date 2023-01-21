c700e84e14241586045141a56cea9d05d8a9cffc
Shubham Lal <111045472+CERTIFIED2003@users.noreply.github.com>
2023-01-21 19:26:46 +0530

    Update spotify.js

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Spotify() {
     return (
         <>
-        <iframe src="https://open.spotify.com/embed/playlist/0WRYlRS9wHe2SuRELgJkPZ" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1EVKuMoAJjoTIw" className="h-full w-full"></iframe>
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX76Wlfdnj7AP" className="h-full w-full"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/3Zlm8TmO6HeEXdRmXPAz6F" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
         </>
     )
 }
