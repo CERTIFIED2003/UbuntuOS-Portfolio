1afd7c7c97275fb5a79bc6da62990fa0b41a08e4
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-23 19:38:59 +0530

    optimize SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactGA from 'react-ga';
 import './App.css'
 import { Portfolio } from './pages'
@@ -7,14 +6,7 @@ const TRACKING_ID = import.meta.env.VITE_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Portfolio />
 }
 
 export default App
